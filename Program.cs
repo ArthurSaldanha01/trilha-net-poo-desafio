df4ae546d8d771be2a7e73ef0a11716dff6af964
Arthur Saldanha <arthursaldanha>
2023-08-23 15:20:17 -0300

    Desafio Concluído git add .

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456","Modelo 1", "111111111",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("1554512","Modelo 2", "22222222",128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
